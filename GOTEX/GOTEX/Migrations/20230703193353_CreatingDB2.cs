9af54676246b513ee826051035cb71988d0f3ae7
Adeola Tijani <adeola.tijani@targetafrica.net>
2023-07-04 22:57:05 +0100

    new commit

---
+++ GOTEX/GOTEX/Migrations/20230703193353_CreatingDB2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GOTEX.Migrations
+{
+    public partial class CreatingDB2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
