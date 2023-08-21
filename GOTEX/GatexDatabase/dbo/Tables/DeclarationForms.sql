347aa2f00243e01dfcfc46daefb5a9f2081d7bad
dammielair <drayprogrammer45@yahoo.com>
2023-08-21 14:16:43 +0100

    --new commit

---
+++ GOTEX/GatexDatabase/dbo/Tables/DeclarationForms.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[DeclarationForms] (
+    [Id]                INT            IDENTITY (1, 1) NOT NULL,
+    [QuarterId]         INT            NOT NULL,
+    [Year]              INT            NOT NULL,
+    [UserId]            NVARCHAR (450) NOT NULL,
+    [ExportVolume]      DECIMAL (18)   NOT NULL,
+    [CrudeTheft]        INT            NOT NULL,
+    [ExportProceedings] INT            NOT NULL,
+    [Bribe]             INT            NOT NULL,
+    [StaffBribe]        NVARCHAR (MAX) NULL,
+    [OutstandingFee]    INT            NOT NULL,
+    [Offence]           INT            NOT NULL,
+    [Violation]         INT            NOT NULL,
+    CONSTRAINT [PK_DeclarationForms] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_DeclarationForms_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
